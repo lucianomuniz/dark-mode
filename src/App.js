b5dc9a3c64f3d22946edbadc895f5d26a9b00534
Luciano Muniz <luciano.mo@gmail.com>
2022-06-17 14:56:06 -0300

    added dark-mode

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+import Article from './Article';
+import data from './data';
+
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
+
+const App = () => {
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>overreacted</h1>
+          <button className='btn' onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className='articles'>
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default App;
